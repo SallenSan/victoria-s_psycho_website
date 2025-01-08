43468c27e9ab9a3060df350471194e365c224c44
Sallenkon S. Almeida <sallenkon54@gmail.com>
2025-01-07 21:19:44 -0300

    starting the project

---
+++ psychological-consultation/src/main/java/com/psycho/psychological_consultation/PsychologicalConsultationApplication.java
@@ -0,0 +1,13 @@
+package com.psycho.psychological_consultation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychologicalConsultationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsychologicalConsultationApplication.class, args);
+	}
+
+}
---
+++ psychological-consultation/src/test/java/com/psycho/psychological_consultation/PsychologicalConsultationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psycho.psychological_consultation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychologicalConsultationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
